ac37cacf8a628c8b37f93e105c4b0f37de068b80
Marcos Fernandes <markbombomb@gmail.com>
2020-06-20 12:45:31 -0300

    try/except on crud functions

--- bookman.py
+++ bookman.py
@@ -26,31 +26,40 @@ class Book(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    books = None
     if request.form:
-        book = Book(title=request.form.get("title"))
-        db.session.add(book)
-        db.session.commit()
-        print(request.form)
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+            print(request.form)
+        except Exception as e:
+            print(f"Failed to add this book: {book.title} - {e}")
     books = Book.query.all()
     return render_template("home.html", books=books)
 
 
 @app.route("/update", methods=["POST"])
 def update():
-    new_title = request.form.get("newtitle")
-    oldtitle = request.form.get("oldtitle")
-    book = Book.query.filter_by(title=oldtitle).first()
-    book.title = new_title
-    db.session.commit()
+    try:
+        new_title = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = new_title
+        db.session.commit()
+    except Exception as e:
+        print(f"Failed to update this book: {book.title} - {e}")
     return redirect("/")
 
 
 @app.route("/delete/<id>", methods=["GET", "POST"])
 def delete(id):
-    book = Book.query.filter_by(id=id).first()
-    db.session.delete(book)
-    db.session.commit()
-    # flash("Employee Deleted Successfully")
+    try:
+        book = Book.query.filter_by(id=id).first()
+        db.session.delete(book)
+        db.session.commit()
+    except Exception as e:
+        print(f"An error occurred during delete process: {e}")
     return redirect("/")
 
 
